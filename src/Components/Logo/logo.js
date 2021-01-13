6ca739a0f835877ef92cd6c13b828c882fbedd6a
edisons3608 <57467707+edisons3608@users.noreply.github.com>
2021-01-13 14:46:30 -0600

    react.js format

---
+++ src/Components/Logo/logo.js
@@ -0,0 +1,10 @@
+import Giorno from "../../images/Giorno.png"
+
+var Logo = function logo(){
+    return(
+        <div>
+            <img src={Giorno} alt = "Giorno"></img>
+        </div>
+    )
+}
+export default Logo;
---
+++ src/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import details from "./details.json";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+var login = document.getElementById("login");
+var username = document.getElementById("username");
+var password = document.getElementById("password");
+function loginClick(){
+  var admins = ["edison","andrew","mao","ethan","andy"];
+  if (username.value !== "" && password.value !== ""){
+    if(admins.includes(username.value.toLowerCase())){
+      if(details[username.value.toLowerCase()] === password.value){
+        alert("memes");
+        //client.channels.get('742121910976118864').send('Hello here!');
+        
+      }
+    }
+  }
+}
+login.addEventListener("click",loginClick);
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
