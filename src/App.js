3d6d342c5e46eadfcf8949c9070449ba09386fc7
edisons3608 <57467707+edisons3608@users.noreply.github.com>
2021-01-13 14:44:27 -0600

    react.js format

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Invite from "./Components/Invite/invite.js";
+import Logo from "./Components/Logo/logo.js";
+import Login from "./Components/Login/login.js";
+function App() {
+  return (
+    <div className="App">
+      <Invite />
+      <Logo />
+      <Login />
+    </div>
+  );
+}
+
+export default App;
